cdd6e56862ddb65766a7c4d28da3d0410ef0e1ba
Hunain Ali <119546006+HunainBaloch@users.noreply.github.com>
2024-04-19 21:49:48 +0500

    New Update

--- model/user_model.py
+++ model/user_model.py
@@ -1,4 +1,8 @@
+import _mysql_connector
+
 class user_model():
-    def user_signup_model(self):
-        #Business Logic
-        return "This is user_signup_model"
+    def __init__(self):
+        # Connection Establishment code
+    def user_getall_model(self):
+        #Qurey execution code
+        return "This is user_getall_model"
