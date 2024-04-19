d5b13ee97e0f3350d64a70c4f4c552658a7d0163
Hunain Ali <119546006+HunainBaloch@users.noreply.github.com>
2024-04-19 22:31:14 +0500

    New Update

--- model/user_model.py
+++ model/user_model.py
@@ -3,6 +3,11 @@ import _mysql_connector
 class user_model():
     def __init__(self):
         # Connection Establishment code
+        try:
+            con = _mysql_connector.connect(host="localhost", user="root",password="",database="flask_tutorial")
+            print("Connection Sucessful")
+        except:
+            print("Some Error")
     def user_getall_model(self):
         #Qurey execution code
         return "This is user_getall_model"
